5ccfb2bbddc4e57448d838f325f8359985dcda9b
voiceinthedark <darkrisingforce@gmail.com>
2025-07-10 12:43:12 +0300

    linux scaffolding

---
+++ index.js
@@ -0,0 +1,21 @@
+const gameBoard = (() => {    
+    const gameState = ['', '', '', '', '', '', '', '', ''];
+
+
+    return {gameState};
+})();
+
+const gameController = (() => {
+    let gameStatus = true; // True for running, false if done
+
+    const makeAMove = (player) => {
+        
+    };
+
+    
+})();
+
+const playerFactory = () => {
+    const player = 'X';
+    const bot = 'O';    
+};
